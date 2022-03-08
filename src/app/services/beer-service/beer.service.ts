f7a04764944d642d13202eb7a8499162b51e8a0c
wearenn <sebastien.philipp57@gmail.com>
2022-03-08 11:30:12 +0100

    error api calling

--- src/app/services/beer-service/beer.service.ts
+++ src/app/services/beer-service/beer.service.ts
@@ -1,9 +1,58 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {BehaviorSubject, Observable} from "rxjs";
+import { Beer } from 'app/models/beer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerService {
 
-  constructor() { }
+  private beers = new BehaviorSubject<string>('');
+
+  private urlServer:any = {};
+
+  constructor(private readonly http: HttpClient) {
+
+    let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
+    if (environment.backend.port) {
+      baseUrl += `:${environment.backend.port}`;
+    }
+
+    // build all backend urls
+    Object.keys(environment.backend.endpoints).forEach(
+      // @ts-ignore
+      k => (this.urlServer[k] = `${baseUrl}${environment.backend.endpoints[k]}`)
+    );
+    console.log(this.urlServer);
+  }
+
+  get beers$(): Observable<string> {
+    return this.beers.asObservable();
+  }
+
+  updatedMusicList(data: string){
+    this.beers.next(data);
+  }
+
+  fetch(): Observable<Beer[]> {
+    return this.http.get<Beer[]>("http://localhost:5001/api/beer");
+  }
+
+  fetchOne(id: string): Observable<Beer> {
+    return this.http.get<Beer>(this.urlServer.uneBiere.replace(':id', id));
+  }
+
+  create(biere: Beer): Observable<Beer> {
+    return this.http.post<Beer>(this.urlServer.toutesLesBieres, biere);
+  }
+
+  update(biere: Beer): Observable<Beer> {
+    return this.http.put<Beer>(this.urlServer.uneBiere.replace(':id', biere.id), biere);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(this.urlServer.uneBiere.replace(':id', id));
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  backend: {
+    protocol: 'http',
+    host: '127.0.0.1',
+    port: '5001',
+    endpoints: {
+      toutesLesBieres: '/api/Beer',
+      uneBiere: '/api/Beer/:id'
+    }
+  }
 };
