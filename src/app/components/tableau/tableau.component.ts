b807b65c9db24e49cab0d5dffbf6501361358855
wearenn <sebastien.philipp57@gmail.com>
2022-03-08 17:19:53 +0100

    ajout tableau sans pagination

--- src/app/components/tableau/tableau.component.ts
+++ src/app/components/tableau/tableau.component.ts
@@ -1,34 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Beer } from 'app/models/beer';
 import { BeerService } from 'app/services/beer-service/beer.service';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { faBeer } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'tableau',
   templateUrl: './tableau.component.html',
-  styleUrls: ['./tableau.component.scss']
+  styleUrls: ['./tableau.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class TableauComponent implements OnInit {
+export class TableauComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
 
   beers: Beer[] = [];
+  faBeer = faBeer;
+  expandedElement!: Beer | null;
   public dataSource = new MatTableDataSource<Beer>();
   displayedColumns: string[] = [
-    'Nom',
-    'Pays',
-    'Type',
-    'Categorie',
-    'Degré',
-    'Volumetrie',
-    'Description'
+    'name',
+    'country',
+    'type',
+    'categories',
+    'degree',
+    'bottle'
   ];
 
   constructor(private readonly beerService : BeerService){}
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
   ngOnInit(): void {
     this.beerService.fetch().subscribe((beers) => {
-      this.beers = beers || [];
+      this.beers = beers;
+      this.dataSource = new MatTableDataSource(this.beers);
     });
-    this.dataSource = new MatTableDataSource(this.beers);
   }
 }
 
